04a0b035be445590df8d4d4045b69e399c3c229c
lcnihao <318082789@qq.com>
2019-06-07 19:39:05 +0800

    计算圆柱的体积

---
+++ 98.c
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: 98.c
+	> Author: lichun
+	> Mail: 318082789@qq.com
+	> Created Time: 2019年06月07日 星期五 19时35分08秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+#define PI 3.14 //acos(-1)
+
+
+int main () {
+    double r, h;
+    scanf("%lf%lf", &r, &h);
+    printf("%.2lf\n", PI * r * r * h);
+    return 0;
+}
