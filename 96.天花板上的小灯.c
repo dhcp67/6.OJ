89a8cf553508f63a24ceaae1a12d6dac07df02e8
lcnihao <318082789@qq.com>
2019-06-07 19:34:53 +0800

    天花板上的灯

--- 96.天花板上的小灯.c
+++ 96.天花板上的小灯.c
@@ -8,8 +8,8 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    scanf("%d%d", &a, &b);
-    printf("%d\n", a * b);
+    long a, b;
+    scanf("%ld%ld", &a, &b);
+    printf("%ld\n", a * b);
     return 0;
 }
