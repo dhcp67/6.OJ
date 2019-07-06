7c1cef89a288fca68c04d848a9117404f696b576
lcnihao <318082789@qq.com>
2019-07-06 15:25:06 +0800

    update

--- 187.c
+++ 187.c
@@ -9,30 +9,36 @@
 #define MAX_N 100
 
 long long cnt[MAX_N + 5] = {0};
+long long num[MAX_N + 5] = {0};
 
-int s = 0;
+long long s = 0;
 
-void move(int n, int x, int y) {
-    //printf("%d %d %d\n", n, x, y);
-    s += n;
+long long add(int n) {
+    if (n == 1) {
+        return n;
+    }
+    num[n] = n + 2 * add(n - 1);
+    return num[n];
 }
 
-int hanno(int n, int one, int two, int three) {
+long long hanno(int n, int one, int two, int three) {
     s += n;
     if (n == 1) {
-        //move(n, one, three);
         cnt[1] = 1;
         return 1;
     }
-    //move(n - 1, one, two);
-    //move(n - 1, two, three);
     long long ret;
     if (cnt[n]) {
         ret = cnt[n];
+        if (num[n]) s += num[n];
+        else {
+            num[n] = 2 * add(n - 1);
+            s += num[n];
+        }
     } else {
         ret = hanno(n - 1, one, three, two);
         ret += hanno(n - 1, one, three, two);
-        //cnt[n] = ret;
+        cnt[n] = ret;
     }
     return 1 + ret;
 }
@@ -42,6 +48,7 @@ int main() {
     int one = 1, two = 2, three = 3;
     scanf("%d", &n);
     long long ans = hanno(n, one, three, two);
-    printf("%lld %d\n", ans, s);
+    printf("%lld %lld\n", ans, s);
     return 0;
 }
+
