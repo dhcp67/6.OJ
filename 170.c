6a7a12b8959de00c3764b41ea495c8ebeda120d9
lcnihao <318082789@qq.com>
2019-08-02 12:55:45 +0800

    updata

--- 170.c
+++ 170.c
@@ -9,60 +9,17 @@
 #include <string.h>
 
 #define MAX_N 1000
-
 char publ[] = "Ban_smoking";
+char sub[] = "No_smoking";
 
-void swap(char *arr, int pos, int size) {
-    int j = 0;
-    int len = strlen(publ);
-    for (int i = pos; i < size; i++) {
-        if (i == pos) {
-            arr[i] = 'N';
-            continue;
-        }
-        else if (i == pos + 1) {
-            arr[i] = 'o';
-            continue;
-    }
-
-        else {
-            if (i == size - 1) {
-                arr[i] = '\0';
-                break;
-            }
-            arr[i] = arr[i + 1];
-        }
-        j++;
-
-    } 
-    return ;
-}
 int main() {
-    int len = strlen(publ);
-    char arr[MAX_N + 5][MAX_N + 5];
     int n;
     scanf("%d", &n);
-    getchar();
-    for (int i = 0; i < n; i++){
-        scanf("%s", arr[i]);
-        getchar();
-    }
-    for (int i = 0; i < n; i++) {
-        int cnt = 0;
-        for (int j = 0; j < strlen(arr[i]); j++) {
-            if (publ[cnt] == arr[i][j]) {
-                cnt++;
-                if (cnt == len) {
-                    swap(arr[i], j - len + 1, strlen(arr[i]));
-                    cnt = 0;
-                }
-            } else {
-                cnt = 0;
-            }
-        }
-    }
-    for (int i = 0; i < n; i++) {
-        printf("%s\n", arr[i]);
+    while (n--) {
+        char buff[MAX_N] = {0};
+        scanf("%s", buff);
+        char *str = strreplace(buff, publ, sub);
+        printf("%s\n", str);
     }
     return 0;
 }
